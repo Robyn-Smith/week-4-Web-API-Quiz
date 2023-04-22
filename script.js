2d02510a635a8ca48dd1e75edd3f056d822d314e
Robyn Smith <robynsmith21@hotmail.com>
2023-04-23 00:13:28 +0100

    started js code

--- script.js
+++ script.js
@@ -33,3 +33,15 @@ var questions = [
       answer: 'console.log',
     },
   ];
+
+  var startQuiz = document.getElementById("startQuiz")
+  // jason reccoments doing just this first
+  //var saveScore = document.getElementById("saveScore")
+  //var viewScore = document.getElementById("viewScore")
+  //var playAgain = document.getElementById("playAgain")
+
+  var welcome = document.getElementById("welcome")
+  var quiz = document.getElementById("quiz")
+  var result = document.getElementById("result")
+
+  var options = document.getElementById("options")

ffbcdc89a6ac9df1ca959dfa92868e601d9ca476
Robyn Smith <robynsmith21@hotmail.com>
2023-04-27 04:15:27 +0100

    deleted unneeded comments

--- highscore.js
+++ highscore.js
@@ -12,33 +12,3 @@ for (var i = 0; i < localStorage.length; i++) {
         
         highscores.appendChild(result);
     }
-
-
-// for (let i = 0; i < localStorage.length; i++)  {      
-//    let key = localStorage.key(i);
-//    console.log(localStorage.getItem(key));
-// }
-
-// //add scores.js for local storage
-// var scoresheet = document.getElementById("scoresheet")
-// var backtoQuiz = document.getElementById("backtoquiz")
-
-// function onBackToQuiz() {
-//     window.location.href = "index.html"; //.............................................
-// }
-
-// for (var i = 0; i < localStorage.length; i++) {
-
-//     var initials = localStorage.key(i);
-//     var score = localStorage.getItem(initials);
-
-//     var result = document.createElement("div");
-//     result.classList.add("result");
-
-//     result.innerHTML = '<div class="score-item">${initials}</div>'
-//             '<div class="score-item">${score}</div>'
-
-//     scoresheet.appendChild(result);
-// }
-
-// backtoQuiz.addEventListener("click", onBackToQuiz);
